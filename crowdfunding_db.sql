35a59587b0309cc0ee6aee27730861ece228ca72
miguelmolinaa <molina.miguelaa@gmail.com>
2023-09-07 18:58:45 -0500

    adding SQL

--- crowdfunding_db.sql
+++ crowdfunding_db.sql
@@ -1,40 +1,58 @@
--- Contacts (users) table
-CREATE TABLE contacts (
-    contact_id INT PRIMARY KEY,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+CREATE TABLE "campaigns" (
+    "cf_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(255)   NOT NULL,
+    "description" TEXT   NOT NULL,
+    "goal" DECIMAL   NOT NULL,
+    "pledged" DECIMAL   NOT NULL,
+    "outcome" VARCHAR(255)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" VARCHAR(255)   NOT NULL,
+    "currency" VARCHAR(3)   NOT NULL,
+    "launch_date" DATE   NOT NULL,
+    "end_date" DATE   NOT NULL,
+    "category_id" VARCHAR(255)   NOT NULL,
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    CONSTRAINT "pk_campaigns" PRIMARY KEY (
+        "contact_id","category_id","subcategory_id"
+     )
 );
 
--- Categories table
-CREATE TABLE categories (
-    category_id VARCHAR(255) PRIMARY KEY,
-    category VARCHAR(255) NOT NULL
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
+
+    CONSTRAINT "uc_contacts_email" UNIQUE (
+        "email"
+    )
 );
 
--- Subcategories table
-CREATE TABLE subcategories (
-    subcategory_id VARCHAR(255) PRIMARY KEY,
-    subcategory VARCHAR(255) NOT NULL
+CREATE TABLE "categories" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL
 );
 
--- Campaigns table
-CREATE TABLE campaigns (
-    cf_id INT PRIMARY KEY,
-    contact_id INT REFERENCES contacts(contact_id),
-    company_name VARCHAR(255) NOT NULL,
-    description TEXT,
-    goal DECIMAL NOT NULL,
-    pledged DECIMAL NOT NULL,
-    outcome VARCHAR(255) NOT NULL,
-    backers_count INT NOT NULL,
-    country VARCHAR(255) NOT NULL,
-    currency VARCHAR(3) NOT NULL,
-    launch_date DATE NOT NULL,
-    end_date DATE NOT NULL,
-    category_id VARCHAR(255) REFERENCES categories(category_id),
-    subcategory_id VARCHAR(255) REFERENCES subcategories(subcategory_id)
+CREATE TABLE "subcategories" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL
 );
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaigns" ("contact_id");
+
+ALTER TABLE "categories" ADD CONSTRAINT "fk_categories_category_id" FOREIGN KEY("category_id")
+REFERENCES "campaigns" ("category_id");
+
+ALTER TABLE "subcategories" ADD CONSTRAINT "fk_subcategories_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "campaigns" ("subcategory_id");
 
 SELECT * FROM contacts LIMIT 10;
 SELECT * FROM categories LIMIT 10;
--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -2,35 +2,8 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "contacts" (
-    "contact_id" INT   NOT NULL,
-    "first_name" VARCHAR(255)   NOT NULL,
-    "last_name" VARCHAR(255)   NOT NULL,
-    "email" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_contacts" PRIMARY KEY (
-        "contact_id"
-     ),
-    CONSTRAINT "uc_contacts_email" UNIQUE (
-        "email"
-    )
-);
-
-CREATE TABLE "categories" (
-    "category_id" VARCHAR(255)   NOT NULL,
-    "category" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_categories" PRIMARY KEY (
-        "category_id"
-     )
-);
-
-CREATE TABLE "subcategories" (
-    "subcategory_id" VARCHAR(255)   NOT NULL,
-    "subcategory" VARCHAR(255)   NOT NULL,
-    CONSTRAINT "pk_subcategories" PRIMARY KEY (
-        "subcategory_id"
-     )
-);
-
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 CREATE TABLE "campaigns" (
     "cf_id" INT   NOT NULL,
     "contact_id" INT   NOT NULL,
@@ -47,16 +20,37 @@ CREATE TABLE "campaigns" (
     "category_id" VARCHAR(255)   NOT NULL,
     "subcategory_id" VARCHAR(255)   NOT NULL,
     CONSTRAINT "pk_campaigns" PRIMARY KEY (
-        "cf_id"
+        "contact_id","category_id","subcategory_id"
      )
 );
 
-ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_contact_id" FOREIGN KEY("contact_id")
-REFERENCES "contacts" ("contact_id");
+CREATE TABLE "contacts" (
+    "contact_id" INT   NOT NULL,
+    "first_name" VARCHAR(255)   NOT NULL,
+    "last_name" VARCHAR(255)   NOT NULL,
+    "email" VARCHAR(255)   NOT NULL,
 
-ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_category_id" FOREIGN KEY("category_id")
-REFERENCES "categories" ("category_id");
+    CONSTRAINT "uc_contacts_email" UNIQUE (
+        "email"
+    )
+);
 
-ALTER TABLE "campaigns" ADD CONSTRAINT "fk_campaigns_subcategory_id" FOREIGN KEY("subcategory_id")
-REFERENCES "subcategories" ("subcategory_id");
+CREATE TABLE "categories" (
+    "category_id" VARCHAR(255)   NOT NULL,
+    "category" VARCHAR(255)   NOT NULL
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" VARCHAR(255)   NOT NULL,
+    "subcategory" VARCHAR(255)   NOT NULL
+);
+
+ALTER TABLE "contacts" ADD CONSTRAINT "fk_contacts_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "campaigns" ("contact_id");
+
+ALTER TABLE "categories" ADD CONSTRAINT "fk_categories_category_id" FOREIGN KEY("category_id")
+REFERENCES "campaigns" ("category_id");
+
+ALTER TABLE "subcategories" ADD CONSTRAINT "fk_subcategories_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "campaigns" ("subcategory_id");
 
